6c94f84d0d564a381f1bffdde7c6c23ebf8bd696
vanceleon <vanceleon44@gmail.com>
2022-01-12 09:01:05 -0500

    Initial commit

---
+++ test/myapi.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Myapi = require('../lib/myapi-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/myapi-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Myapi.MyapiStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
